f9ad9d5a1161cf4065730b62b70450eb41dae24b
pyankita <pyankitaghartimagar@gmail.com>
2025-05-28 21:05:29 +0545

    form added

--- finance_tracker/views.py
+++ finance_tracker/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from finance_tracker.forms import RegisterForm
+from django.contrib.auth import login
+
 
 class RegisterView(View):
     def get(self,request,*args,**kwargs):
         form = RegisterForm()
         return render (request,'register.html',
         {"form":form},)
+    
+    def post(self,request,*args,**kwargs):
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            login(request,user)
+            redirect("")
