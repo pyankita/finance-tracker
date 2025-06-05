d23867bf0ec65224655d842704d0b594e3af06af
pyankita <pyankitaghartimagar@gmail.com>
2025-06-05 21:00:33 +0545

    templates

--- finance_tracker/models.py
+++ finance_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(choices=[('income', 'Income'), ('expense', 'Expense')], max_length=10)
+    category = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
