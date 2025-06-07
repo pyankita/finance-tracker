9f4102db0c55e7197eed4bd99b41afd80c962c91
pyankita <pyankitaghartimagar@gmail.com>
2025-06-07 09:37:20 +0545

    update,delete

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("",views.TransactionListView.as_view(),name="transaction-list"),
     path("detail/<int:pk>/",views.TransactionDetailView.as_view(),name="transaction-detail"),
     path("create/",views.TransactionCreateView.as_view(),name="transaction-create"),
+    path("delete/<int:pk>/",views.TransactionDeleteView.as_view(),name="transaction-delete"),
+    path("update/<int:pk>/",views.TransactionUpdateView.as_view(),name="transaction-update"),
 
 ]
--- finance_tracker/views.py
+++ finance_tracker/views.py
@@ -2,7 +2,9 @@ from django.urls import reverse_lazy
 from .models import Transaction
 from finance_tracker.forms import RegisterForm
 from django.contrib.auth import login
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+from django.db.models import Sum
+
 
 
 class TransactionListView(ListView):
@@ -10,6 +12,35 @@ class TransactionListView(ListView):
     template_name="transaction_list.html"
     context_object_name='transactions'
     ordering=['-date']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        transaction_type = self.request.GET.get('type')  # e.g. income or expense
+        start_date = self.request.GET.get('start_date')  # e.g. 2025-06-01
+        end_date = self.request.GET.get('end_date')      # e.g. 2025-06-07
+
+        if transaction_type in ['income', 'expense']:
+            queryset = queryset.filter(type=transaction_type)
+        
+        if start_date:
+            queryset = queryset.filter(date__gte=start_date)
+        
+        if end_date:
+            queryset = queryset.filter(date__lte=end_date)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        income = Transaction.objects.filter(type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+        expense = Transaction.objects.filter(type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+        balance = income - expense
+
+        context['total_income'] = income
+        context['total_expense'] = expense
+        context['balance'] = balance
+        return context
 
 class TransactionDetailView(DetailView):
     model=Transaction
@@ -21,3 +52,15 @@ class TransactionCreateView(CreateView):
     template_name="transaction_create.html"
     fields="__all__"
     success_url=reverse_lazy('transaction-list')
+
+class TransactionDeleteView(DeleteView):
+    model=Transaction
+    template_name="transaction_delete.html"
+    success_url=reverse_lazy("transaction-list")
+
+
+class TransactionUpdateView(UpdateView):
+    model=Transaction
+    template_name="transaction_update.html"
+    fields="__all__"
+    success_url=reverse_lazy("transaction-list")
